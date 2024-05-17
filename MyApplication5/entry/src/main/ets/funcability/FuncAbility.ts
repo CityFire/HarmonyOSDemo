2ffe417dfcba7988c18c1deac3f7ea861eb251b4
CityFire <13787697167@163.com>
2024-05-17 23:42:25 +0800

    启动UIAbility的指定页面

--- MyApplication5/entry/src/main/ets/funcability/FuncAbility.ts
+++ MyApplication5/entry/src/main/ets/funcability/FuncAbility.ts
@@ -3,6 +3,15 @@ import hilog from '@ohos.hilog';
 import window from '@ohos.window';
 
 export default class FuncAbility extends UIAbility {
+  funcAbilityWant;
+
+  // 当被调用方Ability的启动模式设置为multiton启动模式时，每次启动都会创建一个新的实例，那么onNewWant()回调就不会被用到。
+  onNewWant(want, launchParam) {
+    // 接收调用方UIAbility传过来的参数
+    globalThis.funcAbilityWant = want;
+    // ...
+  }
+
   onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
     // AbilityB从globalThis读取name并输出
@@ -12,8 +21,11 @@ export default class FuncAbility extends UIAbility {
     // globalThis.context = this.context;
 
     // 接收调用方UIAbility传过来的参数
-    let funcAbilityWant = want;
-    let info = funcAbilityWant?.parameters?.info;
+    this.funcAbilityWant = want;
+
+    globalThis.funcAbilityWant = want;
+
+    let info = this.funcAbilityWant?.parameters?.info;
     // ...
   }
 
@@ -25,7 +37,14 @@ export default class FuncAbility extends UIAbility {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
 
-    windowStage.loadContent('pages/Page', (err, data) => {
+    let url = 'pages/FuncPage';
+    if (this.funcAbilityWant?.parameters?.router) {
+      if (this.funcAbilityWant.parameters.router === 'funcA') {
+        url = 'pages/FuncPage';
+      }
+    }
+
+    windowStage.loadContent(url, (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
         return;
